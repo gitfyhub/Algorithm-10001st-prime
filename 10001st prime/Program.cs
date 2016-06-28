70dc8378d4a4d5a5dd2d941b1d711fb2460e359a
gitfyhub <fyounus99@gmail.com>
2016-06-28 01:24:58 -0500

    initial commit

---
+++ 10001st prime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/*
+10001st prime
+Published on Friday, 28th December 2001, 06:00 pm; Solved by 270789; Difficulty rating: 5%
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+
+namespace _10001st_prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            for (int i = 1; i < 2000000; i++)
+            {
+
+                if (isPrime3(i))
+                {
+
+                    if (count == 10001)
+                    {
+                        Console.WriteLine("{0}", i);
+                        break;
+                    }
+
+                    count++;
+                }
+
+            }
+        }
+
+        public static bool isPrime3(int number)
+        {
+            if (number < 2) return false;
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+
+    }
+}
